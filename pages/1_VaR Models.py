d657a160bea4fa1e1153ec0d899da95bb5840974
LouisBerluraud <berluraudlouis@gmail.com>
2025-12-30 16:04:07 +0100

    Update 1_VaR Models.py

--- pages/1_VaR Models.py
+++ pages/1_VaR Models.py
@@ -8,8 +8,7 @@ import seaborn as sns
 from scipy.stats import norm
 
 # Importing tickers from a csv
-# tickers_1 = pd.read_csv("/Users/pafornwl/Desktop/Code/Python/Streamlit/Projet2/tickers.csv", sep= ';') # Guillaume
-tickers_1 = pd.read_csv("C:/Users/berlu/OneDrive/Bureau/Projet PM/1/tickers.csv", sep=";") # Louis
+tickers_1 = pd.read_csv("tickers.csv", sep=";") 
 tickers_1 = tickers_1.set_index('Symbol')
 
 # Function to import data from Yahoo Finance
